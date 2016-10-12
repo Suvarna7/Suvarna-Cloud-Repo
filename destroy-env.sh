bab1589e60c503e1a22fe7d9f6ac5eb19f5fd190
Suvarna7 <sjulania@hawk.iit.edu>
2016-10-12 06:39:47 +0000

    version2 destroy

--- destroy-env.sh
+++ destroy-env.sh
@@ -2,11 +2,12 @@
 echo "Deleting Instances"
 Instance=`aws ec2 describe-instances --filters "Name=instance-state-code,Values=16" --query 'Reservations[*].Instances[*].InstanceId'`
 
+aws autoscaling detach-load-balancers --load-balancer-names ITMO544sj --auto-scaling-group-name server-rg
+aws autoscaling set-desired-capacity update-auto-scaling-group-name server-rg --min-size 0 --desired-capacity 0
 aws ec2 wait instance-terminated --instance-ids $Instance
 
-echo "Instances deleted"
-echo "Starting Deletion of Auto Scaling group"
-aws autoscaling set-desired-capacity update-auto-scaling-group-name --auto-scaling-group-name server-rg --min-size 0 --desired-capacity 0
+echo "Instances deleted "
+
 aws autoscaling delete-auto-scaling-group --auto-scaling-group-name server-rg
 echo "Starting Deletion of Launch Configuration"
 aws autoscaling delete-launch-configuration --launch-configuration-name  itmo544sjconfigure
